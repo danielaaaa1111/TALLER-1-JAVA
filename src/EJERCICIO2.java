2560090db6d71fa04aaa5e552db94a2dbf09bcf0
danielaaaa1111 <142046996+danielaaaa1111@users.noreply.github.com>
2023-08-22 19:25:23 -0500

    Update EJERCICIO2.java

--- src/EJERCICIO2.java
+++ src/EJERCICIO2.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
 
 public class EJERCICIO2{
+mport java.util.Scanner;
 
-public static void main(String[] args) {
+public class NUMEROLETRA {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Ingrese un carácter: ");
-
-        char caracter2 = sc.next().charAt(0);
-
-
-        if (java.lang.Character.isLetter(caracter2)) {
-        if (java.lang.Character.isUpperCase(caracter2)) {
-
-        System.out.println("El carácter es una letra mayúscula.");
-        } else {
-        System.out.println("El carácter es una letra minúscula.");
-        }
-        } else if (java.lang.Character.isDigit(caracter2)) {
-
-        System.out.println("El carácter es un número.");
-
-        } else {
-
-        System.out.println("El carácter no es una letra ni un número.");
+            System.out.print("Ingrese un carácter: ");
+            char letraNumero = scanner.next().charAt(0);
+            int acsii = (int)letraNumero;
+            if (Character.isLetter(letraNumero)) {
+                    
+                if (letraNumero>=65 && letraNumero<=90) {
+                        
+                    System.out.println("El carácter es una letra mayúscula.");
+                } else {
+                    System.out.println("El carácter es una letra minúscula.");
+                }
+            } else if (letraNumero>=48 && letraNumero<=57) {
+                System.out.println("El carácter es un número.");
+            } else {
+                System.out.println("El carácter no es una letra ni un número.");
+            }
 
         }
+}
 
 
-        }
+        
 
-        }
+        
 
